91380a1ddd35b260384b8666cafc9aa64fd2caea
kyleranous <kranous05@gmail.com>
2023-11-23 12:32:21 -0700

    Finished initial tests for RuleSet class

--- tests/test_rule_set.py
+++ tests/test_rule_set.py
@@ -66,21 +66,125 @@ def test_rule_set_invalid_payload():
     """
     Test that an invalid payload returns False and populates errors as expected
     """
+    validation_rules = {
+        'key1': [r.Max(5)],
+        'key2': [r.length(min=2)]
+    }
 
-    assert False
+    test_dict = {
+        'key1': 9,
+        'key2': "This string will pass"
+    }
+
+    rule_set = RuleSet(validation_rules, test_dict=test_dict)
+
+    assert not rule_set
+    assert rule_set.errors is not None
+    assert 'key1' in rule_set.errors
+    assert 'key2' not in rule_set.errors
 
 
 def test_nested_rule_set_passes():
     """
     Test that a nested ruleset works as expected
     """
+    sub_rules = {
+        'sub_key1': [r.is_type(int)],
+        'sub_key2': [r.length(min=3)]
+    }
+    sub_rule_set = RuleSet(sub_rules)
+    validation_dict = {
+        'key1': [r.not_none()],
+        'key2': sub_rule_set
+    }
+    rule_set = RuleSet(validation_dict)
 
-    assert False
+    test_dict = {
+        'key1': "Hello World!",
+        'key2': {
+            'sub_key1': 3,
+            'sub_key2': [1, 3, 4, 5]
+        }
+    }
+
+    rule_set.test_dict = test_dict
+
+    assert rule_set
+    assert not rule_set.errors
 
 
 def test_nested_rule_set_fails():
     """
     Test that a nested ruleset works as expected
     """
+    sub_rules = {
+        'sub_key1': [r.is_type(int)],
+        'sub_key2': [r.length(min=3)]
+    }
+    sub_rule_set = RuleSet(sub_rules)
+    validation_dict = {
+        'key1': [r.not_none()],
+        'key2': sub_rule_set
+    }
+    rule_set = RuleSet(validation_dict)
 
-    assert False
+    test_dict = {
+        'key1': "Hello World!",
+        'key2': {
+            'sub_key1': 3.7,
+            'sub_key2': [1, 3, 4, 5]
+        }
+    }
+
+    rule_set.test_dict = test_dict
+
+    assert not rule_set
+    assert rule_set.errors is not None
+    assert 'key1' not in rule_set.errors
+    assert 'key2' in rule_set.errors
+    assert 'sub_key1' in rule_set.errors['key2']
+    assert 'sub_key2' not in rule_set.errors['key2']
+
+
+def test_multiple_checks_per_field_valid():
+    """
+    Test that a field with multiple checks that is valid returns expected results
+    """
+
+    validation_dict = {
+        'key1': [r.is_type(str), r.length(max=12)]
+    }
+
+    test_dict = {
+        'key1': "Hello!"
+    }
+
+    rule_set = RuleSet(validation_dict, test_dict=test_dict)
+
+    assert rule_set
+    assert not rule_set.errors
+
+def test_multiple_checks_per_field_fails():
+    """
+    Test that a field with multple checks that has one or more fails, returns expected results
+    """
+
+    validation_dict = {
+        'key1': [r.is_type(str), r.length(max=2)],
+        'key2': [r.is_type(int), r.Max(12)]
+    }
+
+    test_dict = {
+        'key1': 'Hello World!',
+        'key2': 13.4
+    }
+
+    # key1 should fail and length of key1 errors should be 1
+    # key2 should fail and length of key2 errors should be 2
+
+    rule_set = RuleSet(validation_dict, test_dict=test_dict)
+
+    assert not rule_set
+    assert rule_set.errors
+    assert len(rule_set.errors['key1']) == 1
+    assert len(rule_set.errors['key2']) == 2
