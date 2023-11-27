d91d65255028f5434cb1c38a517dd0b9a6295392
Kyle Ranous <kranous05@gmail.com>
2023-11-27 10:01:24 -0700

    Updated required to only check if the key is present in the payload regardless of the value

--- api_toolkit/validate/rules/required.py
+++ api_toolkit/validate/rules/required.py
@@ -11,20 +11,36 @@ class Required(Rule):
     """
 
     def __init__(self, **kwargs) -> None:
+        self._key = None
         self.message = None
+        if 'key' in kwargs:
+            self.key = kwargs.get('key')
         super().__init__(**kwargs)
 
     def check(self):
         """
         Validate that the value is not None or empty
         """
-        if self.value is None or (hasattr(self.value, '__len__') and len(self.value) == 0):
-            self.result = False
-        else:
-            self.result = True
+        self.result = self.key in self.value
 
     def _build_error_message(self) -> None:
         """
         Builds the error message for the rule.
         """
-        self.error = "Required Field: Value cannot be None or Empty"
+        self.error = "Required Field: Key not found"
+
+    @property
+    def key(self) -> str:
+        """
+        gets the key to check
+        """
+        return self._key
+
+    @key.setter
+    def key(self, value: str) -> None:
+        """
+        Sets the key to check
+        """
+        self._key = value
+        if self._key is not None and self.value is not None:
+            self.check()
