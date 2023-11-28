05da555b3b7b8bc94bf4c5e0af119f1c68769a60
kyleranous <kranous05@gmail.com>
2023-11-27 22:18:17 -0700

    Trying again

--- api_toolkit/validate/__init__.py
+++ api_toolkit/validate/__init__.py
@@ -1,6 +1,6 @@
 """
 This is the validation module for the api_toolkit package
 """
+from . import utils as Utils
 from . import rules as Rules
 from .rule_set import RuleSet
-from . import utils as Utils
