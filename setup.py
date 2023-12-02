b866d8ebe7afcde723477965a439287648a6e526
kyleranous <kranous05@gmail.com>
2023-12-02 12:16:19 -0700

    Updated versions to 0.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="api_toolkit",
-    version="0.2.0",
+    version="0.3.0",
     packages=find_packages(),
     install_requires=[
         'emoji',
